0fb8bf4806ddd0ddbc881c86203883b4da6990c5
Michael Alan Stowe <mastowe94@gmail.com>
2023-07-09 14:28:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+      for (let j=i+1; j<array.length;j++){
+          if (array[i] + array[j] === target){
+              return true
+          }
+      }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n²)
+  n the number of times it has to go throught the array
+  n the number of times it has to go through array for each previous n
 */
 
 /* 
   Add your pseudocode here
+  for of loop array
+    for of the rest of array
+      add two to see if equal target
 */
 
 /*
   Add written explanation of your solution here
+  returns true if at any point they add to target. if the loops both finish then the only other option is to return false since the loop was never broken with return
 */
 
 // You can run `node index.js` to view these console logs
